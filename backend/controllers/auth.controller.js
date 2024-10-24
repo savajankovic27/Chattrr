f4ac566884d4a1df7cda0a6866288347c87bd1b1
savajankovic-420 <jankovs@mcmaster.ca>
2024-10-23 20:42:06 -0400

    jwt token possibility added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from '../models/user.model.js';
+import generateTokenAndSetCookie from '../utils/generateToken.js'
 
 export const signup = async (req,res) =>{
   try {
@@ -31,16 +32,22 @@ export const signup = async (req,res) =>{
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic
     });
 
-    await newUser.save();
+    if (newUser){
+       //generate jwt token
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullName:newUser.fullName,
-      username:newUser.username,
-      profilePic:newUser.profilePic
-    });
-  } 
-  catch (error){
+      res.status(201).json({
+        _id: newUser._id,
+        fullName:newUser.fullName,
+        username:newUser.username,
+        profilePic:newUser.profilePic
+      });
+    } else {
+      res.status(400).json({error: "Invalid user data"});
+    }
+
+  } catch (error){
     console.log("Error in signup controller", error.message);
     res.status(500).json({error:"Internal Server Error"});
   }
