99dd3e2583f0589a5615ec3ee216861b5f2db9e7
savajankovic-420 <jankovs@mcmaster.ca>
2024-10-27 18:09:22 -0400

    Update
    Massive update to the authentication controller, added the message message, conversation sub as well.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 import connectToMongoDB from "../db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
 const app = express();
 const port = process.env.port || 5001;
@@ -13,14 +14,13 @@ dotenv.config();
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 
 app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
 
 
 // This will output a "Cannot GET" message if it is commented out. 
 // app.get("/", (req,res) => {
 //     res.send("Hello World!!")
 // })
-
-
 
 // to edit
 app.listen(5001, () => {
