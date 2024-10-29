f9f64bebeb771076e17ac7859209bf4894a846e9
savajankovic-420 <jankovs@mcmaster.ca>
2024-10-29 17:11:41 -0400

    UI Update
    Pretty much finished with everything related to the UI of the application. Moving more into the backend now where I'll be implementing more API's and working on the database

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -6,7 +6,6 @@ const Home = () => {
   return <div className='flex sm:h-[458px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
       <Sidebar />
       <MessageContainer/>
-      {/* <MessageContainer /> */}
   </div>;
 };
 
