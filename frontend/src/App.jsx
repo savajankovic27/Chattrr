c3b7669adef827a2922e85c7df1f186e8b4fb26c
savajankovic-420 <jankovs@mcmaster.ca>
2024-10-30 12:36:42 -0400

    Receiving some errors, will debug later

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {Route, Routes} from 'react-router-dom';
 import './App.css'
 import SignUp from "./pages/signup/SignUp"
 import Home from "./pages/home/home";
+import Toaster from 'react-hot-toast';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path ='/login' element = {<Login/>} />
         <Route path='/signup' element={<SignUp/>} />
       </Routes>
+      <Toaster/>
     </div>
   )
 }
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 import GenderCheckbox from './GenderCheckbox';
 import { Link } from 'react-router-dom';
+import { useState} from 'react';
+import useSignup from '../../hooks/useSignup';
+
+
+
 
 const SignUp = () => {
+    // Obtaining the user input as a constructor
+    const [inputs, setInputs] = useState({
+      fullName: '',
+      username: '',
+      password: '',
+      confirmPassword:'',
+      gender: ''
+    });
+
+    const {loading,signup} = useSignup()
+
+    const handleCheckboxChange = (gender) =>{
+      setInputs({...inputs,gender})
+    }
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      await signup(inputs);
+    }
+
     return (
       <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
         <div className='w-full p-6 rounded-lg shadow-md bg-gray-800 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
@@ -10,38 +35,54 @@ const SignUp = () => {
             Sign Up for <span className='text-blue-900'>Chattrr</span>
           </h1>
   
-          <form>
+          <form onSubmit = {handleSubmit}>
             <div>
               <label className='label p-2'>
                 <span className='text-base label-text text-blue-900 '>Full Name</span>
               </label>
-              <input type="text" placeholder = 'John Doe' className = 'input input-bordered input-primary w-full max-w-xs' />
+              <input type="text" placeholder = 'John Doe' className = 'input input-bordered input-primary w-full max-w-xs' 
+                value={inputs.fullName}
+                // will only update the full name that is coming from the value that the user has put in 
+                onChange={(e) => setInputs({...inputs,fullName: e.target.value})}
+              />
             </div>
 
             <div>
             <label className='label p-2'>
                 <span className='text-base label-text text-blue-900'>Username</span>
                 </label>
-                <input type="text" placeholder = 'johndoe' className = 'input input-bordered input-primary w-full max-w-xs' />
+                <input type="text" placeholder = 'johndoe' className = 'input input-bordered input-primary w-full max-w-xs' 
+                  value = {inputs.username}
+                  // will only update the username in the database
+                  onChange = {(e) => setInputs({...inputs, username: e.target.value})}
+                />
             </div>
 
             <div>
                 <label className='label'>
                     <span className = 'text-base label-text text-blue-900'>Password</span>
                 </label>
-                <input type="text" placeholder = 'Enter Password' className = 'input input-bordered input-primary w-full max-w-xs' />
+                <input type="text" placeholder = 'Enter Password' className = 'input input-bordered input-primary w-full max-w-xs' 
+                  value = {inputs.password}
+                  // will only update the... you guessed it, password that the user inputs for the database
+                  onChange = {(e) => setInputs({...inputs,password: e.target.value})}
+                />
             </div>
 
             <div>
                 <label className='label'>
                     <span className = 'text-base label-text text-blue-900'>Confirm Password</span>
                 </label>
-                <input type="text" placeholder = 'Confirm Password' className = 'input input-bordered input-primary w-full max-w-xs' />
+                <input type="text" placeholder = 'Confirm Password' className = 'input input-bordered input-primary w-full max-w-xs' 
+                  value = {inputs.confirmPassword}
+                  // confirming password
+                  onChange = {(e) => setInputs({...inputs,confirmPassword: e.target.value})}
+                />
             </div>
 
-            <GenderCheckbox/>
+            <GenderCheckbox onCheckboxChange = {handleCheckboxChange} selectedGender = {inputs.gender} />
 
-            <Link to={'/login'} className='text-sm hover:underline hover:text-white-300 mt-2 inline-block'>
+            <Link to={'/login'} className='text-sm hover:underline hover:text-white-300 mt-2 inline-block' href='#'>
                 Already have an account?
             </Link>
             <div>
