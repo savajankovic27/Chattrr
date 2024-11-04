05f861246f14025fad0ae97b046456290ec3b916
savajankovic-420 <jankovs@mcmaster.ca>
2024-11-03 22:38:50 -0500

    User Authentication development

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,25 @@ import Login from "./pages/login/Login";
 import './App.css'
 import SignUp from "./pages/signup/SignUp"
 import Home from "./pages/home/Home";
-import {Routes, Route} from 'react-router-dom';
+import {Navigate, Routes, Route} from 'react-router-dom';
 import {Toaster} from 'react-hot-toast'
+import { useAuthContext } from "./context/AuthContext";
+import { BiNavigation } from "react-icons/bi";
 
+// addition of protected routes. Logout next
 function App() {
+  const {authUser}  = useAuthContext();
   return (
     <div className = 'p-4 h-screen flex items-center justify-center'>
-      <Routes>
-       <Route path='/' element={<Home/>} />
-       <Route path='/login' element={<Login/>}/>
-       <Route path='/signup' element={<SignUp/>}/>
+      <Routes> 
+       <Route path='/' element={authUser ? <Home/> : <Navigate to={"/login"}/>} />
+       <Route path='/login' element={authUser ? <Navigate to="/" /> : <Login/>}/>
+       <Route path='/signup' element={authUser ? <Navigate to="/" /> : <SignUp/>}/> 
       </Routes>
       <Toaster />
     </div>
   )
 }
+// if the user is authenticated, takes to the next supported page. If not, it stays there. 
 
 export default App;
--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import toast from 'react-hot-toast'
 import SignUp from '../pages/signup/SignUp';
+import { useAuthContext } from '../context/AuthContext';
 
 
 const useSignup = () => {
   const [loading,setLoading] = useState(false);
+  const {setAuthUser} = useAuthContext()
 
   const signup = async(fullName,username,password,confirmPassword,gender) => {
     const success = handleInputErrors({fullName,username,password,confirmPassword,gender})
@@ -19,7 +21,14 @@ const useSignup = () => {
       })
 
       const data = await res.json();
-      console.log(data)
+      if(data.error){
+        throw new Error(data.error)
+      }
+      // localstorage
+      localStorage.setItem("chat-user", JSON.stringify(data))
+      // context
+      setAuthUser(data)
+
 
     } catch (error){
       toast.error(error.message);
