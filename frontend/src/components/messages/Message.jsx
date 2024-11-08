4fc0262aac34210e4ec256829efab70e7af60d4e
savajankovic-420 <jankovs@mcmaster.ca>
2024-11-07 22:30:41 -0500

    Proper timestamps

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,12 +1,12 @@
 import { useAuthContext } from "../../context/AuthContext";
-//import { extractTime } from "../../utils/extractTime";
+import { extractTime } from "../../../utils/extractTime";
 import useConversation from "../../zustand/useConversation";
 
 const Message = ({ message }) => {
 	const { authUser } = useAuthContext();
 	const { selectedConversation } = useConversation();
 	const fromMe = message.senderId === authUser._id;
-	//const formattedTime = extractTime(message.createdAt);
+	const formattedTime = extractTime(message.createdAt);
 	const chatClassName = fromMe ? "chat-end" : "chat-start";
 	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
 	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
@@ -24,6 +24,7 @@ const Message = ({ message }) => {
 				</div>
 			</div>
 			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+            <div className='chat-footer opacity-50 text-xs flex gap-1 items-center text-black' >{formattedTime}</div>
 		</div>
 	);
 };
